64d02acb91b8375c6301158d41e2d67cec002961
Saurabh Verma <saurabh1088@gmail.com>
2023-04-16 08:08:57 +0530

    Added new learning project for uikit.

---
+++ UIKitLearnings/UIKitLearnings/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//
+// ViewController.swift
+// UIKitLearnings
+//
+// Created by Saurabh Verma on 15/04/23
+// Copyright © 2023 Saurabh Verma, (saurabh1088@gmail.com). All rights reserved.
+//
+        
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        self.navigationItem.title = "Productivity"
+    }
+
+
+}
+
