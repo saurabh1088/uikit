8dfe4647650b12eed82fed69ab4a2ac99bdb8e0c
Saurabh Verma <saurabh1088@gmail.com>
2023-10-03 23:32:37 +0530

    More changes for login sign in module for VIPER structure.

---
+++ UIKitLearnings/UIKitLearnings/SignIn/Register/RegisterNewUserViewController.swift
@@ -0,0 +1,15 @@
+//
+//  RegisterNewUserViewController.swift
+//  UIKitLearnings
+//
+//  Created by Saurabh Verma on 03/10/23.
+//
+
+import UIKit
+
+class RegisterNewUserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
