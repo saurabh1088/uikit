b651c68008c9f8d6f32e0b68214eafad2eaa0499
Saurabh Verma <saurabh1088@gmail.com>
2023-10-07 00:14:59 +0530

    Updated view to use multiple collection views.

--- UIKitLearnings/UIKitLearnings/UIComponents/UIComponentsViewController.swift
+++ UIKitLearnings/UIKitLearnings/UIComponents/UIComponentsViewController.swift
@@ -7,22 +7,37 @@
 
 import UIKit
 
-class UIComponentsViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        50
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath)
-        return cell
-    }
-    
-
+class UIComponentsViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var secondaryCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.tag = 0
+        secondaryCollectionView.tag = 1
         collectionView.delegate = self
         collectionView.dataSource = self
+        secondaryCollectionView.delegate = self
+        secondaryCollectionView.dataSource = self
+    }
+}
+
+extension UIComponentsViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView.tag == 0 {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "collectionViewCell", for: indexPath)
+            return cell
+        } else {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "secondaryCollectionViewCell", for: indexPath)
+            return cell
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 200, height: 200)
     }
 }
