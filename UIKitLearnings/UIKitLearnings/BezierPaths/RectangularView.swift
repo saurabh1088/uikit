b6e23e0038fe8bcc67ac0a5b9b322c8dee8e5d9f
Saurabh Verma <saurabh1088@gmail.com>
2024-01-19 11:31:33 +0530

    UIKit : Added example using IBDesignable and IBInspectable.

--- UIKitLearnings/UIKitLearnings/BezierPaths/RectangularView.swift
+++ UIKitLearnings/UIKitLearnings/BezierPaths/RectangularView.swift
@@ -7,8 +7,30 @@
 
 import UIKit
 
+/**
+ @IBDesignable is an attribute which when is applied to a UIView or NSView subclass, tells Interface Builder in
+ Xcode to render the view directly in the canvas. This allows seeing how that custom views will appear without
+ building and running the app after each change.
+ 
+ `RectangularView` is used for example in this project in `AnimationsStoryboard`. Notice if `@IBDesignable`
+ is removed from `RectangularView` then in `AnimationsStoryboard` the view using `RectangularView`
+ appears white.
+ However if `@IBDesignable` is added then the storyboard `AnimationsStoryboard` shows the proper
+ color.
+ So when storyboard is viewed with @@IBDesignable then as per default color it will show the rectangle view as
+ red color.
+ 
+ Now the color for `RectangularView` is set using property `rectangleBaseColor`. Observe the difference
+ when this property is annotated with `@IBInspectable`.
+ Without `@IBInspectable` the only way to observe change in color in storyboard for this view is to change
+ the value of this property in code.
+ However once we annotate the property `rectangleBaseColor` with `@IBInspectable` we can observe
+ this property in the Interface Builder's, Attribute inspector, for the storyboard `AnimationsStoryboard`.
+ */
+@IBDesignable
 class RectangularView: UIView {
-    
+        
+    @IBInspectable var rectangleBaseColor: UIColor = UIColor.red
     var path: UIBezierPath!
     
     override init(frame: CGRect) {
@@ -21,7 +43,9 @@ class RectangularView: UIView {
     
     override func draw(_ rect: CGRect) {
         createRectangularShapeUsingBezierPath()
-        UIColor.blue.setFill()
+        /// Calling this setFill() is important here as this will set the color for rectangle view, even if with property
+        /// rectangleBaseColor being @IBInspectable and can be set through Interface Builder.
+        rectangleBaseColor.setFill()
         path.fill()
     }
     
