5adf4702c6bf06ccd03c0a73fc664c3d32a31f68
Saurabh Verma <saurabh1088@gmail.com>
2024-03-02 07:07:16 +0530

    Added activity indicator for button trigerring api.

--- UIKitLearnings/UIKitLearnings/Networking/AlamofireNetworking/AlamofireNetworkingViewController.swift
+++ UIKitLearnings/UIKitLearnings/Networking/AlamofireNetworking/AlamofireNetworkingViewController.swift
@@ -10,23 +10,34 @@ import Alamofire
 
 class AlamofireNetworkingViewController: UIViewController {
 
+    @IBOutlet weak var fetchDataButtonActivityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var fetchDataButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchDataButtonActivityIndicator.isHidden = true
     }
     
     @IBAction func fetchDataButtonAction(_ sender: Any) {
-        AlamofireViewModel.getAuthors()
+        // TODO: Format keeping in mind following MVVM approach
+        fetchDataButton.titleLabel?.isHidden = true
+        fetchDataButtonActivityIndicator.isHidden = false
+        fetchDataButtonActivityIndicator.startAnimating()
+        AlamofireViewModel.getAuthors {
+            self.fetchDataButton.titleLabel?.isHidden = false
+            self.fetchDataButtonActivityIndicator.isHidden = true
+            self.fetchDataButtonActivityIndicator.stopAnimating()
+        }
     }
 }
 
 
 // TODO: Move this to independent file and remove related code
 class AlamofireViewModel {
-    static func getAuthors() {
+    static func getAuthors(completion: @escaping () -> ()) {
         AF.request("https://openlibrary.org/authors/OL1A.json").response { response in
             print("Received Response ::")
             print("\(String(data: response.data!, encoding: .utf8) as AnyObject)")
+            completion()
         }
     }
 }
