4deef78068b099da42b4a36cfc9253d22cd8231d
Saurabh Verma <saurabh1088@gmail.com>
2024-06-04 22:39:08 +0530

    Changes to refactor animations view.

---
+++ UIKitLearnings/UIKitLearnings/Animations/AnimationPlayViewController.swift
@@ -0,0 +1,79 @@
+//
+//  AnimationPlayViewController.swift
+//  UIKitLearnings
+//
+//  Created by Saurabh Verma on 04/06/24.
+//
+
+import UIKit
+
+class AnimationPlayViewController: UIViewController {
+
+    var animationOption: AnimationsOption!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let animatingView = getAnimationView() {
+            self.view.addSubview(animatingView)
+        }
+    }
+    
+    func getAnimationView() -> UIView? {
+        if let animationOption = animationOption {
+            switch animationOption {
+            case .circularProgressView:
+                return setUpCircularProgressView()
+            case .imageView:
+                return setUpAnimatingImageView()
+            case .rectangularView:
+                return setUpRectangularAnimatingView()
+            }
+        }
+        return nil
+    }
+}
+
+extension AnimationPlayViewController {
+
+    func setUpCircularProgressView() -> UIView {
+        let circularProgressAnimationView = CircularProgressView(viewModel: createCircularProgressViewModel(),
+                                                                 frame: CGRect(x: 100, y: 200, width: 200, height: 200))
+        let viewModel = createCircularProgressViewModel()
+        circularProgressAnimationView.viewModel = viewModel
+        circularProgressAnimationView.createCircularBezierPath()
+        circularProgressAnimationView.center = view.center
+        circularProgressAnimationView.progressAnimation(duration: 2)
+        return circularProgressAnimationView
+    }
+
+    func createCircularProgressViewModel() -> CircularProgressViewModel {
+        return CircularProgressViewModel(circlePathLayer: CAShapeLayer(),
+                                         progressViewLayer: CAShapeLayer(),
+                                         startAngle: CGFloat(-Double.pi / 2),
+                                         endAngle: CGFloat(3 * Double.pi / 2),
+                                         animationDuration: 2,
+                                         radius: 60)
+    }
+}
+
+extension AnimationPlayViewController {
+
+    func setUpAnimatingImageView() -> UIView {
+        /// NOTE: Tint color for image works with renderingMode set to alwaysOriginal, if renderingMode is
+        /// set to alwaysTemplate then there is no effect of the tint color passed here.
+        let imageAnimationView = UIImageView(frame: CGRect(x: 100, y: 200, width: 200, height: 200))
+        imageAnimationView.image = UIImage(systemName: "smiley")?.withTintColor(.yellow, renderingMode: .alwaysOriginal)
+        view.addSubview(imageAnimationView)
+        UIView.animate(withDuration: 2) {
+            imageAnimationView.transform = imageAnimationView.transform.rotated(by: CGFloat(Double.pi))
+        }
+        return imageAnimationView
+    }
+}
+
+extension AnimationPlayViewController {
+    
+    func setUpRectangularAnimatingView() -> UIView {
+        return RectangularView(frame: CGRect(x: 100, y: 200, width: 200, height: 200))
+    }
+}
--- UIKitLearnings/UIKitLearnings/Animations/AnimationsViewController.swift
+++ UIKitLearnings/UIKitLearnings/Animations/AnimationsViewController.swift
@@ -6,66 +6,58 @@
 //
 
 import UIKit
+import OSLog
 
-class AnimationsViewController: UIViewController {
+class AnimationsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    @IBOutlet weak var circularProgressAnimationView: CircularProgressView!
-    @IBOutlet weak var imageAnimationView: UIImageView!
-    @IBOutlet weak var rectangularAnimationView: RectangularView!
+    @IBOutlet weak var tableView: UITableView!
+    private var selectedAnimationOption: AnimationsOption?
     
-    var animatingImageView: UIImageView!
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        setUpCircularProgressView()
-        setUpAnimatingImageView()
-        setUpRectangularAnimatingView()
-    }
-    
-    // TODO: ISSUE, this is not working
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        rectangularAnimationView.animateRectangleView()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 }
 
 extension AnimationsViewController {
-    
-    func setUpCircularProgressView() {
-        let viewModel = createCircularProgressViewModel()
-        circularProgressAnimationView.viewModel = viewModel
-        circularProgressAnimationView.createCircularBezierPath()
-        circularProgressAnimationView.center = view.center
-        circularProgressAnimationView.progressAnimation(duration: 2)
-        view.addSubview(circularProgressAnimationView)
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
     }
     
-    func createCircularProgressViewModel() -> CircularProgressViewModel {
-        return CircularProgressViewModel(circlePathLayer: CAShapeLayer(),
-                                         progressViewLayer: CAShapeLayer(),
-                                         startAngle: CGFloat(-Double.pi / 2),
-                                         endAngle: CGFloat(3 * Double.pi / 2),
-                                         animationDuration: 2,
-                                         radius: 60)
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        AnimationsOption.allCases.count
     }
-}
-
-extension AnimationsViewController {
     
-    func setUpAnimatingImageView() {
-        /// NOTE: Tint color for image works with renderingMode set to alwaysOriginal, if renderingMode is
-        /// set to alwaysTemplate then there is no effect of the tint color passed here.
-        imageAnimationView.image = UIImage(systemName: "smiley")?.withTintColor(.yellow, renderingMode: .alwaysOriginal)
-        view.addSubview(imageAnimationView)
-        UIView.animate(withDuration: 2) {
-            self.imageAnimationView.transform = self.imageAnimationView.transform.rotated(by: CGFloat(Double.pi))
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let animationOption = AnimationsOption.optionFor(index: indexPath.row) {
+            let cell = tableView.dequeueReusableCell(withIdentifier: animationOption.cellReUseIdentifier, for: indexPath)
+            cell.textLabel?.text = animationOption.rawValue
+            cell.textLabel?.textColor = UIColor.white
+            // By default selected cell will be highlighted with a color greyish. To turn
+            // this behaviour OFF, set selectionStyle as none
+            cell.selectionStyle = .none
+            return cell
         }
+        return UITableViewCell()
     }
 }
 
 extension AnimationsViewController {
-    
-    func setUpRectangularAnimatingView() {
-        view.addSubview(rectangularAnimationView)
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        Logger.uiTableViewDelegateCallbacks.info("AnimationsViewController tableView didSelectRowAt \(indexPath)")
+        selectedAnimationOption = AnimationsOption.optionFor(index: indexPath.row)
+        self.performSegue(withIdentifier: "showAnimationPlayViewController", sender: self)
+    }
+}
+
+extension AnimationsViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showAnimationPlayViewController" {
+            if let viewController = segue.destination as? AnimationPlayViewController,
+               let selectedOption = selectedAnimationOption {
+                viewController.animationOption = selectedOption
+            }
+        }
     }
 }
