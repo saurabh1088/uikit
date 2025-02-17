c4cb0c5835ebd9196c67357da589fb2836e5028f
Saurabh Verma <saurabh1088@gmail.com>
2025-02-17 22:52:44 +0530

    Added logic to manipulate accessibility focus back to button from where alert was trigerred.

--- UIKitLearnings/UIKitLearnings/UIComponents/TableViewCellExamplesViewController.swift
+++ UIKitLearnings/UIKitLearnings/UIComponents/TableViewCellExamplesViewController.swift
@@ -54,7 +54,7 @@ extension TableViewCellExamplesViewController: UITableViewDataSource {
             let image = UIImage(systemName: "exclamationmark.triangle")
             cell.configure(with: title, message: message, icon: image) { [weak self] in
                 // Dismiss action
-                self?.showDismissConfirmation()
+                self?.showDismissConfirmation(cell.dismissButtonControl)
             } actionButtonAction: {
                 // Action button action
                 print("User tapped the action button")
@@ -72,7 +72,7 @@ extension TableViewCellExamplesViewController: UITableViewDelegate {
 }
 
 extension TableViewCellExamplesViewController {
-    private func showDismissConfirmation() {
+    private func showDismissConfirmation(_ sender: UIButton) {
         let alertController = UIAlertController(title: "Confirm Dismiss", message: "Are you sure you want to dismiss this item?", preferredStyle: .alert)
         
         let yesAction = UIAlertAction(title: "Yes", style: .destructive) { _ in
@@ -80,7 +80,20 @@ extension TableViewCellExamplesViewController {
         }
         alertController.addAction(yesAction)
         
-        let noAction = UIAlertAction(title: "No", style: .cancel, handler: nil)
+        let noAction = UIAlertAction(title: "No", style: .cancel, handler: { _ in
+            DispatchQueue.main.async { [weak self] in
+                self?.tableView.isAccessibilityElement = false
+                self?.navigationController?.navigationBar.isAccessibilityElement = false
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                    UIAccessibility.post(notification: .layoutChanged, argument: sender)
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5, execute: {
+                        self?.tableView.isAccessibilityElement = true
+                        self?.navigationController?.navigationBar.isAccessibilityElement = true 
+                    })
+                })
+            }
+            
+        })
         alertController.addAction(noAction)
         
         present(alertController, animated: true, completion: nil)
