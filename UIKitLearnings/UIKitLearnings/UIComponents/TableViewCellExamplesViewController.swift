70f436d6e8e5bfc3f6257edb2ad6ff75ffb63f6a
Saurabh Verma <saurabh1088@gmail.com>
2025-02-17 21:38:00 +0530

    Changes to integrate and use DismissableTitleMessageTableViewCell in examples.

--- UIKitLearnings/UIKitLearnings/UIComponents/TableViewCellExamplesViewController.swift
+++ UIKitLearnings/UIKitLearnings/UIComponents/TableViewCellExamplesViewController.swift
@@ -25,28 +25,64 @@ class TableViewCellExamplesViewController: UIViewController {
         /// Passing nil for bundle here will give below runtime error as SingleDetailTableViewCell nib is part of package dependency
         /// Terminating app due to uncaught exception 'NSInternalInconsistencyException', reason: 'Could not load NIB in bundle: 'NSBundle
         tableView.register(UINib(nibName: "SingleDetailTableViewCell", bundle: bundle), forCellReuseIdentifier: "singleDetailTableViewCellId")
+        tableView.register(UINib(nibName: "DismissableTitleMessageTableViewCell", bundle: bundle), forCellReuseIdentifier: "dismissableTitleMessageTableViewCellId")
     }
 }
 
 extension TableViewCellExamplesViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        1
+        2
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "singleDetailTableViewCellId", for: indexPath) as? SingleDetailTableViewCell else {
-                fatalError("The dequeued cell is not an instance of SingleDetailTableViewCell.")
+        if indexPath.row == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "singleDetailTableViewCellId", for: indexPath) as? SingleDetailTableViewCell else {
+                    fatalError("The dequeued cell is not an instance of SingleDetailTableViewCell.")
+            }
+            cell.configureDetailLabel(text: "Single Detail Table View Cell",
+                                      font: .systemFont(ofSize: 18, weight: .bold),
+                                      textColor: .blue,
+                                      backgroundColor: .darkGray)
+            return cell
+            
+        } else if indexPath.row == 1 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "dismissableTitleMessageTableViewCellId", for: indexPath) as? DismissableTitleMessageTableViewCell else {
+                    fatalError("The dequeued cell is not an instance of DismissableTitleMessageTableViewCell.")
+            }
+            let title = "Title"
+            let message = "Detailed message"
+            let image = UIImage(systemName: "exclamationmark.triangle")
+            cell.configure(with: title, message: message, icon: image) { [weak self] in
+                // Dismiss action
+                self?.showDismissConfirmation()
+            } actionButtonAction: {
+                // Action button action
+                print("User tapped the action button")
+            }
+            return cell
         }
-        cell.configureDetailLabel(text: "Single Detail Table View Cell",
-                                  font: .systemFont(ofSize: 18, weight: .bold),
-                                  textColor: .blue,
-                                  backgroundColor: .darkGray)
-        return cell
+        return UITableViewCell()
     }
 }
 
 extension TableViewCellExamplesViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         88.0
+    }
+}
+
+extension TableViewCellExamplesViewController {
+    private func showDismissConfirmation() {
+        let alertController = UIAlertController(title: "Confirm Dismiss", message: "Are you sure you want to dismiss this item?", preferredStyle: .alert)
+        
+        let yesAction = UIAlertAction(title: "Yes", style: .destructive) { _ in
+            print("User confirmed to dismiss item")
+        }
+        alertController.addAction(yesAction)
+        
+        let noAction = UIAlertAction(title: "No", style: .cancel, handler: nil)
+        alertController.addAction(noAction)
+        
+        present(alertController, animated: true, completion: nil)
     }
 }
